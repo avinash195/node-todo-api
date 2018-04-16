5293538cc12e609cf80c7eb4039a8b4328fedd2e
avinash195 <avinashsngh735@gmail.com>
2018-04-16 07:40:33 +0530

    added get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -9,23 +10,41 @@ let app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-  let todo = new Todo({
-    text: req.body.text
-  })
+// app.post('/todos', (req, res) => {
+//   let todo = new Todo({
+//     text: req.body.text
+//   })
 
-  todo.save().then((doc) => {
-    res.send(doc);
+//   todo.save().then((doc) => {
+//     res.send(doc);
+//   }, (err) => {
+//     res.status(400).send(err);
+//   })
+// })
+
+app.get('/todos/', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send(todos);
   }, (err) => {
     res.status(400).send(err);
   })
 })
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
-    res.send(todos);
-  }, (err) => {
-    res.status(400).send(err);
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
   })
 })
 
